ba82d21fbf50a77e348c0e9f8a7c6071146774a4
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-06-09 08:28:45 -0400

    Cambios en solucion

---
+++ EjemploParcialTest1/BLL/EmpleadoBLLTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimerParcialEjemplo.BLL;
+using PrimerParcialEjemplo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimerParcialEjemplo.BLL.Tests
+{
+    [TestClass()]
+    public class EmpleadoBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Empleado empleado = new Empleado();
+            empleado.empleadoId = 0;
+            empleado.nombre = "Pedro";
+            empleado.ocupacion = 2;
+            empleado.sueldo = 2500;
+            empleado.fecha = DateTime.Now;
+            bool saved = EmpleadoBLL.Guardar(empleado);
+            Assert.AreEqual(saved, true);
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
