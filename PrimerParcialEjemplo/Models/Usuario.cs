7e5eca5e88dbdd5d301df5165ecb95a22865683d
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-06-24 18:27:27 -0400

    Creando validacion..

--- PrimerParcialEjemplo/Models/Usuario.cs
+++ PrimerParcialEjemplo/Models/Usuario.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,17 @@ namespace PrimerParcialEjemplo.Models
 {
     public class Usuario
     {
+        [Key]
+        [Required(ErrorMessage = "Este campo no puede estar vacio.")]
+        [Range(0, 1000, ErrorMessage = ("El rango es de 0 a 1000"))]
         public int usuarioId { get; set; }
+        [Required(ErrorMessage = "Este campo no puede estar vacio.")]
+        [MinLength(3, ErrorMessage = "El minimo de caracteres es 3")]
+        [MaxLength(30, ErrorMessage = "El maximo de caracteres es 30")]
         public string nombre { get; set; }
+        [Required(ErrorMessage = "Este campo no puede estar vacio.")]
         public int nivel { get; set; }
+        [Required(ErrorMessage = "Este campo no puede estar vacio.")]
         public string contraseña { get; set; }
 
         public Usuario()
